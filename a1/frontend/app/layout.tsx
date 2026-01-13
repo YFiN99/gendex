12709bb259ef594254699bd74bab5448d1d9a106
maxi_dak <79904448+YFiN99@users.noreply.github.com>
2026-01-13 08:57:55 +0700

    Fix import statement for Providers component

--- a1/frontend/app/layout.tsx
+++ a1/frontend/app/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-import { Providers } from "./providers"; // Kita akan buat file ini setelah ini
+import { Providers } from "./providers";
 
 export const metadata = {
   title: "GenDEX - Swap",
